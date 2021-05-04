78ed0166ea158550b4ee013f3c5bda9a69211238
IevgenLavrukhin <lavrukhin@gwu.edu>
2021-05-04 15:15:46 -0500

    changed name of track allignment

---
+++ Tracker_Alignment/Real/Track_Alignment_Trk.C
@@ -0,0 +1,27 @@
+#include "G4_InsensitiveVolumes.C"
+#include "G4_SensitiveDetectors.C"
+#include "G4_Beamline.C"
+#include "G4_Target.C"
+#include <TSystem.h>
+#include "Alignment/Alignment.h"
+
+
+
+using namespace std;
+
+
+int Track_Alignment_Real()
+{
+
+
+	printf("Starting the Initialization ... \n");
+
+	
+
+
+
+	return 0; 
+}
+
+
+
